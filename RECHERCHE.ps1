5551e58510b8c9863c2fe1493edb8ca57bff3ae5
Adri-Charbonneau <105881448+Adri-Charbonneau@users.noreply.github.com>
2022-03-21 09:03:06 +0100

    Update RECHERCHE.ps1

--- RECHERCHE.ps1
+++ RECHERCHE.ps1
@@ -2,8 +2,8 @@
 #$code = ([xml](Get-Content MAILLES.xml)).MAILLES.$id
 $list = $list = Get-Content ID.txt
 
-foreach ($code in $list) {
-$id = ([xml](Get-Content ID.xml)).ID."id$code"
+#foreach ($code in $list) {
+$id = ([xml](Get-Content ID.xml)).ID.id42510 #"id$code"
 
 Invoke-WebRequest -Uri "https://oiseauxdefrance.org/api/v1/area/taxa_list/$code" -OutFile "odf.json"
 (Get-Content 'odf.json' -Encoding UTF8 | ConvertFrom-Json).all_period.new_count | Out-File count.csv -Encoding UTF8
@@ -32,7 +32,14 @@ $txt = $txt -replace '^$',''
 
 $txt | Out-File SPECIES1.txt -Encoding UTF8 
 Import-Csv SPECIES1.txt -delimiter ";" -Header newcount , frname | Export-Csv ./SPECIES/CSV/SPECIES-$id.csv -Delimiter ";" -NoTypeInformation -Encoding UTF8
-}
+#}
+
+# git and create tag
+git config --local user.email "a-d-r-i@outlook.fr"
+git config --local user.name "A-d-r-i"
+git add .
+git commit -m "[Bot] Update files - MAILLES"
+git push -f
 
 Remove-Item odf.json, count.csv, name.csv, csvtemp1.txt, csvtemp2.csv, SPECIES1-$id.txt
 
