944ed3bcb8859c57347f6a625cb88dbf6bf15501
Adri-Charbonneau <adrien.charbonneau@free.fr>
2022-03-24 11:52:48 +0100

    Update RECHERCHE.ps1

--- RECHERCHE.ps1
+++ RECHERCHE.ps1
@@ -7,17 +7,23 @@ $code = '42510'
 $id = ([xml](Get-Content ID.xml)).ID."id$code"
 
 Invoke-WebRequest -Uri "https://oiseauxdefrance.org/api/v1/area/taxa_list/$code" -OutFile "odf.json"
-(Get-Content 'odf.json' -Encoding UTF8 | ConvertFrom-Json).all_period.new_count | Out-File count.csv -Encoding UTF8
+(Get-Content 'odf.json' -Encoding UTF8 | ConvertFrom-Json).all_period.last_obs | Out-File lastobs.csv -Encoding UTF8
+(Get-Content 'odf.json' -Encoding UTF8 | ConvertFrom-Json).all_period.new_count | Out-File newcount.csv -Encoding UTF8
+(Get-Content 'odf.json' -Encoding UTF8 | ConvertFrom-Json).all_period.old_count | Out-File oldcount.csv -Encoding UTF8
 (Get-Content 'odf.json' -Encoding UTF8 | ConvertFrom-Json).common_name_fr | Out-File name.csv -Encoding UTF8
 
-$count = Get-Content count.csv -Encoding UTF8
 $name = Get-Content name.csv -Encoding UTF8
+$lastobs = Get-Content lastobs.csv -Encoding UTF8
+$newcount = Get-Content newcount.csv -Encoding UTF8
+$oldcount = Get-Content oldcount.csv -Encoding UTF8
 
-$loop = for ($i = 0; $i -lt $count.Length; ++$i) { $count[$i] + "," + $name[$i] }
-$loop | Out-File csvtemp1.txt -Encoding UTF8
-Import-Csv "csvtemp1.txt" -delimiter "," -Header newcount , frname | Export-Csv SPECIES1.csv -Delimiter "," -NoTypeInformation -Encoding UTF8
-$result = Get-Content "SPECIES1.csv" | Select-String '^\"0\"'
+$loop = for ($i = 0; $i -lt $name.Length; ++$i) { $name[$i] + "," + $lastobs[$i] + "," + $newcount[$i] + "," + $oldcount[$i] }
+$loop | Out-File csvtemp.txt -Encoding UTF8
+Import-Csv "csvtemp.txt" -delimiter "," -Header frname , lastobs , newcount , oldcount | Export-Csv SPECIESTEMP.csv -Delimiter "," -NoTypeInformation -Encoding UTF8
+$search = Import-Csv -Path 'SPECIESTEMP.csv' -delimiter ","
+$result = $search | Where { $_.newcount -eq 0} 
 $result | Out-File ./SPECIES/TXT/SPECIES-$id.txt -Encoding UTF8
+$result | Export-Csv ./SPECIES/CSV/SPECIES-$id.csv -Delimiter "," -NoTypeInformation -Encoding UTF8
 
 echo $id
 #echo ''
@@ -26,10 +32,9 @@ echo $id
 #echo '----------------------------------------------------------------------------'
 #echo ''
 
-Import-Csv ./SPECIES/TXT/SPECIES-$id.txt -delimiter "," -Header newcount , frname | Export-Csv ./SPECIES/CSV/SPECIES-$id.csv -Delimiter "," -NoTypeInformation -Encoding UTF8
 #}
 
-Remove-Item odf.json, count.csv, name.csv, csvtemp1.txt, csvtemp2.csv, SPECIES1.txt
+Remove-Item odf.json, name.csv, lastobs.csv, newcount.csv, oldcount.csv, csvtemp.txt, SPECIESTEMP.csv
 
 # git and create tag
 git config --local user.email "a-d-r-i@outlook.fr"
