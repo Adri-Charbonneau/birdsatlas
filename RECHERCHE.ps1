205ef6cd40ae61385431695db72346e5106060ad
Adri-Charbonneau <105881448+Adri-Charbonneau@users.noreply.github.com>
2022-03-24 11:00:30 +0100

    Update RECHERCHE.ps1

--- RECHERCHE.ps1
+++ RECHERCHE.ps1
@@ -2,8 +2,8 @@
 #$code = ([xml](Get-Content MAILLES.xml)).MAILLES.$id
 $list = $list = Get-Content ID.txt
 
-foreach ($code in $list) {
-#$code = '42510'
+#foreach ($code in $list) {
+$code = '42510'
 $id = ([xml](Get-Content ID.xml)).ID."id$code"
 
 Invoke-WebRequest -Uri "https://oiseauxdefrance.org/api/v1/area/taxa_list/$code" -OutFile "odf.json"
@@ -30,12 +30,12 @@ echo $id
 $txt = Get-Content -path ./SPECIES/TXT/SPECIES-$id.txt -Raw
 $txt = $txt -replace '([0-9]+)        ','$1;'
 $txt = $txt -replace 'newcount frname',''
-$txt = $txt -replace '\n-------- -------',''
+$txt = $txt -replace '-------- ------',''
 $txt = $txt -replace '^$',''
 
 $txt | Out-File SPECIES1.txt -Encoding UTF8 
 Import-Csv SPECIES1.txt -delimiter ";" -Header newcount , frname | Export-Csv ./SPECIES/CSV/SPECIES-$id.csv -Delimiter ";" -NoTypeInformation -Encoding UTF8
-}
+#}
 
 Remove-Item odf.json, count.csv, name.csv, csvtemp1.txt, csvtemp2.csv, SPECIES1.txt
 
@@ -45,4 +45,3 @@ git config --local user.name "A-d-r-i"
 git add .
 git commit -m "[Bot] Update files - MAILLES"
 git push -f
-
