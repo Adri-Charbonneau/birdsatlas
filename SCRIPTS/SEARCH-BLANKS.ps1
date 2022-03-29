8adac3f55d56e6c2a17a58ac899d4da88f988a8c
Adri-Charbonneau <adrien.charbonneau@free.fr>
2022-03-29 19:01:26 +0200

    Test scripts

--- SCRIPTS/SEARCH-BLANKS.ps1
+++ SCRIPTS/SEARCH-BLANKS.ps1
@@ -1,7 +1,7 @@
 $list = Get-Content "../ID.txt"
 
-foreach ($code in $list) {
-#$code = '42510'
+#foreach ($code in $list) {
+$code = '42510'
 $id = ([xml](Get-Content "../ID.xml")).ID."id$code"
 
 # compare with blanks
@@ -10,7 +10,7 @@ $BLANKS.Vernaculaire | ?{$TOTAL.frname -notcontains $_} | Out-File "../SPECIES/L
 ## not found in BLANKS.csv
 $TOTAL.frname | ?{$BLANKS.Vernaculaire -notcontains $_} | Out-File "../SPECIES/LIST/SPECIES-NF-$id.txt"
 
-}
+#}
 
 # git and create tag
 git config --local user.email "a-d-r-i@outlook.fr"
